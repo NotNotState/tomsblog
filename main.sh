f098b134f90e706bce4518aed0260449d1ad6e4a
Thomas Hegarty <tommyhegarty97@gmail.com>
2025-08-13 12:33:20 -0400

    Added src dir and files

---
+++ main.sh
@@ -0,0 +1,2 @@
+#using uv so this is only required to pass the cli tests
+python3 src/main.py
---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode
+
+def main():
+    test_node = TextNode("Some example text", "link", "https://www.boot.deev")
+    print(test_node)
+
+if __name__ == "__main__":
+    main()
